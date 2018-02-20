ef6f0a9a10d3b7a8f7d613debc7bd81e0eebcfd1
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 22:39:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(strings)
-  sorted_strings = strings.sort {|a, b| a.length <=> b.length}
+  strings.sort {|a, b| a.length <=> b.length}
 end
 
 def swap_elements(array)
